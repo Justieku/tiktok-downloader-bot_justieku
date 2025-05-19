a84bb38c05cb84d8d834e898e08169c6454f615b
Некита <74873340+Justieku@users.noreply.github.com>
2025-05-19 12:05:24 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-
-if ! command -v tmux &> /dev/null; then
-    sudo apt update && sudo apt install -y tmux
-fi
-
-tmux new -s tt "python3 tt_download_bot.py"
+python3 tt_download_bot.py
